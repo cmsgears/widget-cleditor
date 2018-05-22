9868d76f02889e654e94fc8320bed87d2d797cf2
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-22 17:59:25 +0530

    Applied quick fixes.

--- ClEditor.php
+++ ClEditor.php
@@ -1,13 +1,23 @@
 <?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
 namespace cmsgears\widgets\cleditor;
 
 // Yii Imports
 use yii\web\View;
 
 // CMG Imports
+use cmsgears\core\common\base\Widget;
+
 use cmsgears\widgets\cleditor\assets\ClEditorAssets;
 
-class ClEditor extends \cmsgears\core\common\base\Widget {
+class ClEditor extends Widget {
 
 	// Variables ---------------------------------------------------
 
--- assets/ClEditorAssets.php
+++ assets/ClEditorAssets.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
 namespace cmsgears\widgets\cleditor\assets;
 
 // Yii Imports
@@ -44,7 +52,7 @@ class ClEditorAssets extends AssetBundle {
 
 	// Define dependent Asset Loaders
     public $depends = [
-		'cmsgears\core\common\assets\Jquery'
+		'cmsgears\assets\jquery\Jquery'
     ];
 
 	// Protected --------------
